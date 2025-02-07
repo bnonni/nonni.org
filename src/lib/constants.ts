6764b7c9dbea31ea0eec7c7074584ba45009c4e7
bnonni <github@nonni.dev>
2025-02-07 11:22:37 -0500

    update resume links

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const LINKS = {
   RESUME: {
-    VIEW: "https://docs.google.com/document/d/1xXt_g7KQsRJzcS18x30DLwJyFIsvG8FP4GK9xg6SSws/edit?format=pdf",
-    DOWNLOAD: "https://docs.google.com/document/d/1xXt_g7KQsRJzcS18x30DLwJyFIsvG8FP4GK9xg6SSws/export?format=pdf",
+    VIEW: "https://docs.google.com/document/d/1AFSR0y6VmChUuDtfe47srpiZW63USZrZyGIJ5tA1CVs/edit?format=pdf",
+    DOWNLOAD: "https://docs.google.com/document/d/1AFSR0y6VmChUuDtfe47srpiZW63USZrZyGIJ5tA1CVs/export?format=pdf",
   }
 };
